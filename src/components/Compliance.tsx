cc1fd09b0f0e1a4efeda31273ee9ce3f42edbf7b
Nicholas Herrington <nicholas.herrington.astro@gmail.com>
2025-01-23 15:02:18 +0000

    Updates

--- src/components/Compliance.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Compliance() {
-  return <div>NO SCREEN DESIGNED YET</div>;
-}
-
-export default Compliance;
--- src/components/Support.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Support() {
-  return <div>NO SCREEN DESIGNED YET</div>;
-}
-
-export default Support;
